962455b18e609c27ab200cbe406c2bd1c5eb9a1f
balejoc <bralioiac@gmail.com>
2024-07-03 16:07:47 -0600

    Clases de servicio y controller

--- src/main/java/com/saihon/Spring/controller/UsuariosController.java
+++ src/main/java/com/saihon/Spring/controller/UsuariosController.java
@@ -1,5 +1,20 @@
 package com.saihon.Spring.controller;
 
-public class UsuariosController {
+import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saihon.Spring.model.Usuario;
+
+@RestController
+@RequestMapping(path="/api/usuarios/")
+public class UsuariosController {
+	
+	
+	@GetMapping
+	public List<Usuario> getUsuarios(){
+		return
+	}
 }
