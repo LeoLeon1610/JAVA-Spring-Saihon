0987d46010a73f4c269d9e9ecfea2718eb7a67d2
Sam <frostrik@gmail.com>
2024-07-03 19:13:27 -0600

    Fixed some stuff

--- src/main/java/com/saihon/Spring/controller/PedidoController.java
+++ src/main/java/com/saihon/Spring/controller/PedidoController.java
@@ -2,17 +2,8 @@ package com.saihon.Spring.controller;
 
 import java.util.ArrayList;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.saihon.Spring.model.Pedido;
 import com.saihon.Spring.service.PedidoService;
@@ -20,39 +11,38 @@ import com.saihon.Spring.service.PedidoService;
 @RestController
 @RequestMapping(path="/api/pedidos/")
 public class PedidoController {
-	private final PedidoService pedidoService;
-	
-	@Autowired
-	public PedidoController(PedidoService pedidoService) {
-		this.pedidoService = pedidoService;
-	}//constructor
-	
-	@GetMapping
-	public ArrayList<Pedido>getAllProducts(){
-		return pedidoService.getAllProducts();
-	}//getProductos
-	
-	@GetMapping(path="{prodId}") //http://localhost:8080/api/pedidos/1
-	public Pedido getPedido(@PathVariable("prodId") int idPedidos) {
-		return pedidoService.getPedido(idPedidos);
-	}//getProducto
-	
-	@DeleteMapping(path="{prodId}") //http://localhost:8080/api/pedidos/1
-	public Pedido deletePedido(@PathVariable("prodId") int idPedidos) {
-		return pedidoService.deletePedido(idPedidos);
-	}//deletePedido
-	
-	@PostMapping
-	public Pedido addPedido(@RequestBody Pedido pedido) {
-		return pedidoService.addPedido(pedido);
-	}
-	
-	@PutMapping
-	public Pedido updatePedido(@PathVariable ("prodId") int idPedidos,
-		@RequestParam(required = false) String domicilio,
-		@RequestParam(required = false) String pago,
-		@RequestParam(required = false) int idUsuario){
-			return pedidoService.updatePedido(idPedidos, domicilio, pago, idUsuario);
-		}//Updtate
-	
-}//classPedidoController
+    private final PedidoService pedidoService;
+
+    @Autowired
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @GetMapping
+    public ArrayList<Pedido> getAllProducts() {
+        return pedidoService.getAllProducts();
+    }
+
+    @GetMapping(path="{prodId}")
+    public Pedido getPedido(@PathVariable("prodId") int idPedidos) {
+        return pedidoService.getPedido(idPedidos);
+    }
+
+    @DeleteMapping(path="{prodId}")
+    public Pedido deletePedido(@PathVariable("prodId") int idPedidos) {
+        return pedidoService.deletePedido(idPedidos);
+    }
+
+    @PostMapping
+    public Pedido addPedido(@RequestBody Pedido pedido) {
+        return pedidoService.addPedido(pedido);
+    }
+
+    @PutMapping(path="{prodId}")
+    public Pedido updatePedido(@PathVariable("prodId") int idPedidos,
+                               @RequestParam(required = false) String domicilio,
+                               @RequestParam(required = false) String pago,
+                               @RequestParam(required = false) int idUsuario) {
+        return pedidoService.updatePedido(idPedidos, domicilio, pago);
+    }
+}
--- src/main/java/com/saihon/Spring/service/PedidoService.java
+++ src/main/java/com/saihon/Spring/service/PedidoService.java
@@ -2,78 +2,63 @@ package com.saihon.Spring.service;
 
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.saihon.Spring.model.Pedido;
 
 @Service
 public class PedidoService {
-	public final ArrayList<Pedido> lista = new ArrayList<Pedido>();
-	
-	@Autowired
-	public PedidoService(){
-		lista.add(new Pedido("Villa Bilbao #140 San Sebastian"," Tarjeta de debito ", "AC2996S"));
-		lista.add(new Pedido("Adelitas #690 Valleverde"," Tarjeta de debito ", "IG3467D"));
-		lista.add(new Pedido("El rosal #2687 Valle de las Flores"," Tarjeta de credito", "PA0610A"));
-		lista.add(new Pedido("Inglaterra #285 Europa"," Tarjeta de debito ", "CM3471C"));
-	}//listaDePedidos
-	
-	public ArrayList<Pedido>getAllProducts(){
-		return lista;
-	}//getAllPorducts
-	
-	public Pedido getPedido(int idPedidos) {
-		Pedido tPedido= null;
-		for (Pedido pedido: lista) {
-			if(pedido.getIdPedidos()==idPedidos) {
-				tPedido = pedido;
-				break;
-			}//if
-		}//forEach
-		return tPedido;
-	}//getPedido
-	
-	public Pedido deletePedido(int idPedidos) {
-		Pedido tPedido = null;
-		for (Pedido pedido : lista) {
-			if (pedido.getIdPedidos()==idPedidos) {
-				tPedido = lista.remove(lista.indexOf(pedido));
-				break;
-			}//if
-		}//forEach
-		return tPedido;
-	}//deletePedido
-	
-	public Pedido addPedido(Pedido pedido) {
-		Pedido tPedido = null;
-		boolean existe = false;
-		for(Pedido prod : lista) {
-			//if(prod.getIdPedidos().equals(pedido.getIdPedidos())) {
-				if (prod.getIdPedidos() == pedido.getIdPedidos()) {
-				existe = true;
-				break;
-			}//if
-		}//forEach
-		if(! existe) {
-			lista.add(pedido);
-			tPedido=pedido;
-		}//if ! existe
-		return tPedido;
-	}//addPedido
+    private final ArrayList<Pedido> lista = new ArrayList<>();
 
-	public Pedido updatePedido(int idPedidos, String domicilio, String pago, int idUsuario) {
-		Pedido tPedido = null;
-		for (Pedido pedido : lista) {
-			if(pedido.getIdPedidos()==idPedidos) {
-				if(domicilio!=null)pedido.setDomicilio(domicilio);
-				if(pago!=null)pedido.setPago(pago);
-				tPedido = pedido;
-				break;
-			}//if
-		}//forEach
-		return tPedido;
-	}//updatePedido
-	
-	
-}//class PedidoService
+    public PedidoService() {
+        lista.add(new Pedido("Villa Bilbao #140 San Sebastian", "Tarjeta de debito", 2996));
+        lista.add(new Pedido("Adelitas #690 Valleverde", "Tarjeta de debito", 3467));
+        lista.add(new Pedido("El rosal #2687 Valle de las Flores", "Tarjeta de credito", 610));
+        lista.add(new Pedido("Inglaterra #285 Europa", "Tarjeta de debito", 3471));
+    }
+
+    public ArrayList<Pedido> getAllProducts() {
+        return lista;
+    }
+
+    public Pedido getPedido(int idPedidos) {
+        for (Pedido pedido : lista) {
+            if (pedido.getIdPedidos() == idPedidos) {
+                return pedido;
+            }
+        }
+        return null;
+    }
+
+    public Pedido deletePedido(int idPedidos) {
+        Pedido tPedido = null;
+        for (Pedido pedido : lista) {
+            if (pedido.getIdPedidos() == idPedidos) {
+                tPedido = lista.remove(lista.indexOf(pedido));
+                break;
+            }
+        }
+        return tPedido;
+    }
+
+    public Pedido addPedido(Pedido pedido) {
+        for (Pedido prod : lista) {
+            if (prod.getIdPedidos() == pedido.getIdPedidos()) {
+                return null; // Pedido ya existe
+            }
+        }
+        lista.add(pedido);
+        return pedido;
+    }
+
+    public Pedido updatePedido(int idPedidos, String domicilio, String pago) {
+        for (Pedido pedido : lista) {
+            if (pedido.getIdPedidos() == idPedidos) {
+                if (domicilio != null) pedido.setDomicilio(domicilio);
+                if (pago != null) pedido.setPago(pago);
+                return pedido;
+            }
+        }
+        return null;
+    }
+}
