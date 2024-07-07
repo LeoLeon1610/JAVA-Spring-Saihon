b11f66b83a83fae986c67bd6268293dacc3b5141
balejoc <bralioiac@gmail.com>
2024-07-06 20:17:29 -0600

    Relación Libros-Categorias

--- src/main/java/com/saihon/Spring/model/Categoria.java
+++ src/main/java/com/saihon/Spring/model/Categoria.java
@@ -1,10 +1,15 @@
 package com.saihon.Spring.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 //POJO - Plain Old Java Object
@@ -18,7 +23,9 @@ public class Categoria {
 	private long id;
 	@Column(name="nombre",nullable=false)
 	private String nombre;
-	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="Categoria_idCategoria", referencedColumnName = "idCategoria")
+	List<Libros> libros_en_categoria = new ArrayList<Libros>();
 	
 	
 	public Categoria(String nombre) {
@@ -42,6 +49,10 @@ public class Categoria {
 		this.nombre = nombre;
 	}
 
+	
+	public List<Libros> getLibros_en_categoria() {
+		return libros_en_categoria;
+	}
 
 	@Override
 	public String toString() {
