cb8f1a297399e29d29c694d5dc0e6098b716159e
Leonardo <leonardo.leonc16@gmail.com>
2024-07-10 12:55:15 -0600

    se arreglaron las rutas de request

--- src/main/java/com/saihon/Spring/Application.java
+++ src/main/java/com/saihon/Spring/Application.java
@@ -18,7 +18,10 @@ public class Application {
 		FilterRegistrationBean<JwtFilter> registrationBean = 
 				new FilterRegistrationBean<JwtFilter>();
 		registrationBean.setFilter(new JwtFilter());
-		registrationBean.addUrlPatterns("/api/productos/*");
+		registrationBean.addUrlPatterns("/api/libroPedido/*");
+		registrationBean.addUrlPatterns("/api/categorias/*");
+		registrationBean.addUrlPatterns("/api/pedidos/*");
+		registrationBean.addUrlPatterns("/api/libros/*");
 		registrationBean.addUrlPatterns("/api/usuarios/*");
 		return registrationBean;
 	}
--- src/main/java/com/saihon/Spring/config/Swagger2Config.java
+++ src/main/java/com/saihon/Spring/config/Swagger2Config.java
@@ -1,6 +1,5 @@
 package com.saihon.Spring.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
