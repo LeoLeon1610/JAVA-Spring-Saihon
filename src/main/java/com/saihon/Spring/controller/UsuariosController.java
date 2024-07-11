40d61cb9a028f5edf4950fc9b47c93bf5a88ce98
Leonardo <leonardo.leonc16@gmail.com>
2024-07-11 00:10:44 -0600

    se agrego la conexion con login y registro

--- src/main/java/com/saihon/Spring/controller/UsuariosController.java
+++ src/main/java/com/saihon/Spring/controller/UsuariosController.java
@@ -3,6 +3,7 @@ package com.saihon.Spring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.saihon.Spring.dto.ChangePassword;
 import com.saihon.Spring.model.Usuario;
 import com.saihon.Spring.service.UsuarioService;
 
+@CrossOrigin(origins = "http://127.0.0.1:5502/")
 @RestController
 @RequestMapping(path="/api/usuarios/")
 public class UsuariosController {
